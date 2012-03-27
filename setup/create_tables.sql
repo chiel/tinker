ddd5820a437bc812e92ac55c419b51ce0d87e5f6
DJ Lee <djlee@spotify.com>
2012-03-27 11:30:34 -0400

    Include initial SQL files for install
    Update README to mention SQL steps and clarify an issue that bit me
    with the config file.

---
+++ setup/create_tables.sql
@@ -0,0 +1,61 @@
+CREATE TABLE tinker (
+  hash CHAR(5) NOT NULL UNIQUE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  PRIMARY KEY (hash)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE tinker_revision (
+  x_tinker_hash CHAR(5) NOT NULL,
+  revision INT(10) NOT NULL DEFAULT 0,
+  x_doctype_id INT(10) NOT NULL,
+  x_framework_version_id int(10) NOT NULL,
+  normalize BOOL NOT NULL,
+  markup TEXT NOT NULL,
+  style TEXT NOT NULL,
+  interaction TEXT NOT NULL,
+  PRIMARY KEY (x_tinker_hash, revision)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE tinker_revision_asset (
+  x_tinker_hash CHAR(5) not null,
+  revision INT(10) not null,
+  url VARCHAR(255) not null
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE tinker_revision_extension (
+  x_tinker_hash CHAR(5) not null,
+  revision INT(10) not null,
+  x_framework_extension_id int (10) not null
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE doctype (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  code TEXT NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE framework (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL,
+  dirname VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE framework_version (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  x_framework_id int(10) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  dirname VARCHAR(255) NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
+
+CREATE TABLE framework_extension (
+  id int(10) NOT NULL AUTO_INCREMENT,
+  x_framework_version_id int(10) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  filename VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB DEFAULT CHARSET 'utf8';
